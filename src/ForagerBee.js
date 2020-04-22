03fab6787f003836b2c9e3b88fc42b5daf38285d
Amanda Winkles <amandawinkles@gmail.com>
2020-04-21 20:34:07 -0500

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-var ForagerBee = function() {
+var ForagerBee = function(food, eat, color) {
+  Bee.call(this, food, eat, color);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = 'yellow';
+  this.canFly= true;
+  this.treasureChest = [];
+};
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
 };
 
