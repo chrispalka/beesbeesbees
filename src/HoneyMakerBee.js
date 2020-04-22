5b57dfa8ee62c371d77e9bf484d378b0c1611f44
Amanda Winkles <amandawinkles@gmail.com>
2020-04-21 20:26:24 -0500

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(food, eat, color) {
+  Bee.call(this, food, eat, color);
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = 'yellow';
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot--;
+}
 
